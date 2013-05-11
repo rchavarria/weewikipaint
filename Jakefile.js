14ac8e62c8d30ef59ce464c6a4f06b74db98618b
rchavarria <txingo@gmail.com>
2013-05-11 20:40:45 +0200

    Configured Lint to analize .js files. Finished episode 2

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,28 @@
-task("default", [], function() {
-	console.log("default");
-})
+/*global desc, task, jake, fail, complete */
 
-desc("this is a description");
-task("example", ["dependency"], function() {
-	console.log("this is an example");
+"use strict";
+
+task("default", ["lint"]);
+
+desc("Lint everything");
+task("lint", [], function() {
+	var lint = require("./build/lint/lint_runner.js");
+
+	var files = new jake.FileList();
+	files.include("**/*.js");
+	files.exclude("node_modules");
+
+	var options = {
+		node: true
+	};
+
+	var globals = {
+		describe: false,
+		it: false,
+		beforeEach: false,
+		afterEach: false
+	};
+
+	lint.validateFileList(files.toArray(), options, globals);
 });
 
-task("dependency", function() {
-	console.log("exe the dependency");
-})
