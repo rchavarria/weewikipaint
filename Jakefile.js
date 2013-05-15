ffce77b9c5a45877adb33c83e451275d50e43f77
rchavarria <txingo@gmail.com>
2013-05-15 16:43:01 +0200

    changes related to continuous integration episode

--- Jakefile.js
+++ Jakefile.js
@@ -12,11 +12,7 @@ task("lint", [], function() {
 	files.include("**/*.js");
 	files.exclude("node_modules");
 
-	lint.validateFileList(files.toArray(), nodeLintOptions(), {});
-});
-
-function nodeLintOptions() {
-	return {
+	var options = {
 		bitwise: true,
 		curly: false,
 		eqeqeq: true,
@@ -33,5 +29,22 @@ function nodeLintOptions() {
 		trailing: true,
 		node: true
 	};
-}
+
+	var passed = lint.validateFileList(files.toArray(), options, {});
+	if(!passed) fail("Lint failed");
+});
+
+desc("Integrate");
+task("integrate", ["default"], function() {
+	console.log("1. make sure 'git status' is clean");
+	console.log("2. build on the integration box");
+	console.log("    a. walk over integration box");
+	console.log("    b. 'git pull' // para coger los últimos cambios en 'master'");
+	console.log("    c. 'jake' con el default task");
+	console.log("    d. if jake fails, stop! tray again after fixing the issue.");
+	console.log("3. 'git checkout integration'");
+	console.log("4. 'git merge master --no-ff --log' // para llevar los cambios que funcionan a la rama 'integration'");
+	console.log("5. 'git checkout master' //para volver al curro en master");
+});
+
 })();
