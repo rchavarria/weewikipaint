d728051d6e1a932a7786d0136ad330c25680994f
rchavarria <txingo@gmail.com>
2013-05-22 23:27:05 +0200

    I got a custom 404 page. episode 22 done

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -9,14 +9,20 @@
 	var fs = require("fs");
 	var assert = require("assert");
 	
-	var TEST_FILE = "generated/test.html";
+	var TEST_HOME_PAGE = "generated/test.html";
+	var TEST_404_PAGE = "generated/test404.html";
 
 	exports.tearDown = function(done) {
-		if(fs.existsSync(TEST_FILE)){
-			fs.unlinkSync(TEST_FILE);
-			assert.ok(!fs.existsSync(TEST_FILE));
-		}
+		cleanUp(TEST_HOME_PAGE);
+		cleanUp(TEST_404_PAGE);
 		done();
+	};
+
+	var cleanUp = function(file) {
+		if(fs.existsSync(file)){
+			fs.unlinkSync(file);
+			assert.ok(!fs.existsSync(file));
+		}
 	};
 
 	exports.test_serverRequiresFileToServe = function(test) {
@@ -26,15 +32,22 @@
 		test.done();
 	};
 
-	exports.test_serverRequiresPortNumber = function(test) {
+	exports.test_serverRequiresHomePage = function(test) {
 		test.throws(function() {
-			server.start(TEST_FILE);
+			server.start();
+		});
+		test.done();
+	};
+
+	exports.test_serverRequires404Pate = function(test) {
+		test.throws(function() {
+			server.start(TEST_HOME_PAGE);
 		});
 		test.done();
 	};
 
 	exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
-		server.start(TEST_FILE, 8080);
+		server.start(TEST_HOME_PAGE, TEST_404_PAGE, 8080);
 		server.stop(function() {
 			test.done();
 		});
@@ -49,7 +62,7 @@
 
 	exports.test_serverServesHomePageFromFile = function(test) {
 		var fileData = "This is served from a file";
-		fs.writeFileSync(TEST_FILE, fileData);
+		fs.writeFileSync(TEST_HOME_PAGE, fileData);
 
 		httpGet("http://localhost:8080", function(response, responseText) {
 			test.equals(response.statusCode, 200, "status code");
@@ -59,24 +72,29 @@
 	};
 
 	exports.test_serverServesHomePageToIndex = function(test) {
-		var fileData = "This is served from a file";
-		fs.writeFileSync(TEST_FILE, fileData);
+		var fileData = "This is the home page";
+		fs.writeFileSync(TEST_HOME_PAGE, fileData);
 
 		httpGet("http://localhost:8080/index.html", function(response, responseText) {
 			test.equals(response.statusCode, 200, "status code");
+			test.equals(responseText, fileData, "response text");
 			test.done();
 		});
 	};
 
 	exports.test_serverResponds404ToEverythingButHomePage = function(test) {
+		var fileData = "This is a custom 404 page";
+		fs.writeFileSync(TEST_404_PAGE, fileData);
+
 		httpGet("http://localhost:8080/bargle", function(response, responseText) {
 			test.equals(response.statusCode, 404, "status code");
+			test.equals(responseText, fileData, "response text");
 			test.done();
 		});
 	};
 
 	function httpGet(url, callback) {
-		server.start(TEST_FILE, 8080);
+		server.start(TEST_HOME_PAGE, TEST_404_PAGE, 8080);
 
 		var request = http.get(url);
 		request.on("response", function(response) {
--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,38 @@
 /* server.js */
+(function() {
 "use strict";
 
 var http = require("http");
 var fs = require("fs");
 var server;
 
-exports.start = function(htmlFileToServe, port) {
+exports.start = function(homePageToServe, notFoundPageToServe, port) {
 	if(!port) throw new Error("port number is required");
 
 	server = http.createServer();
 
 	server.on("request", function(request, response) {
 		if(request.url === "/" || request.url === "/index.html") {
-			fs.readFile(htmlFileToServe, function(err, data) {
-				if(err) throw err;
-				response.end(data);
-			});
+			response.statusCode = 200;
+			serveFile(homePageToServe, response);
 		} else {
 			response.statusCode = 404;
-			response.end();
+			serveFile(notFoundPageToServe, response);
 		}
 	});
 
 	server.listen(port);
 };
 
+var serveFile = function(file, response) {
+	fs.readFile(file, function(err, data) {
+		if(err) throw err;
+		response.end(data);
+	});
+};
+
 exports.stop = function(callback) {
 	server.close(callback);
 };
+
+})();
