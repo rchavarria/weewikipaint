5b93e15445651f95f58e22b44b7dbc7bbaf0d3b8
rchavarria <txingo@gmail.com>
2013-05-23 22:57:29 +0200

    smoke test passes but it does nothing

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -2,10 +2,13 @@
 	"use strict";
 
 	var child_process = require("child_process");
-
+	var http = require("http");
+	
 	exports.test_for_smoke = function(test) {
 		runServer("node src/server/weewikipaint 8080", function() {
-			test.done();
+			httpGet("http://localhost:8080", function() {
+				test.done();
+			})
 		});
 	};
 
@@ -20,5 +23,21 @@
 			callback();
 		});
 	};
+
+	function httpGet(url, callback) {
+		var request = http.get(url);
+		request.on("response", function(response) {
+			var responseText = "";
+			response.setEncoding("utf8");
+
+			response.on("data", function(chunk) {
+				responseText += chunk;
+			});
+			
+			response.on("end", function() {
+				callback(response, responseText);
+			});
+		});
+	}
 
 })();
