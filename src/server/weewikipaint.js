f1fa0cfa85969a04bb1ea23c9f0fa5106bbbda5f
rchavarria <txingo@gmail.com>
2013-07-08 22:36:56 +0200

    extracted a common directory to a constant

--- src/server/weewikipaint.js
+++ src/server/weewikipaint.js
@@ -1,12 +1,14 @@
 (function() {
 	"use strict";
 
+	var HOME_DIR = "src/client";
+
 	var server = require("./server.js");
 
 	// getting the port from the command line arguments ('process' is provided by node)
 	var port = process.argv[2];
 
-	server.start("src/client/homepage.html", "src/client/404.html", port, function() {
+	server.start(HOME_DIR + "/homepage.html", HOME_DIR + "/404.html", port, function() {
 		console.log("Server started");
 	});
 }());
