37aa97e4445b7857d56c8c44d9f4bd31bf805b72
rchavarria <txingo@gmail.com>
2013-07-20 23:54:22 +0200

    first test for DomElement

--- src/client/_dom_element_test.js
+++ src/client/_dom_element_test.js
@@ -1,12 +1,28 @@
-/* global describe, it, $, jQuery, expect, dump, Raphael */
+/* global describe, beforeEach, it, $, jQuery, expect, dump, Raphael, wwp */
 
 (function () {
 "use strict";
 
 describe("DOM Element", function() {
 
-	it("should nothing", function() {
-		expect("foo").to.be("bar");
+	describe("event handlers", function() {
+		var domElement;
+
+		beforeEach(function() {
+			domElement = new wwp.DomElement( $("<div></div>") );
+		});
+
+		it("handles mouse down", function() {
+			var eventOffset = null;
+			domElement.onMouseDown(function(offset, event) {
+				eventOffset = offset;
+			});
+			domElement.mouseDown(42, 13);
+
+			expect(eventOffset).to.eql( {x: 42, y: 13} );
+
+		});
+
 	});
 
 });
