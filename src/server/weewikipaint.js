75cc633f0af263f68e76dd8663236bd4b97aab64
rchavarria <txingo@gmail.com>
2013-07-27 15:44:07 +0200

    installed 'send' and 'phantomjs', but phantomjs is ignored. it is too large

--- src/server/weewikipaint.js
+++ src/server/weewikipaint.js
@@ -8,7 +8,7 @@
 	// getting the port from the command line arguments ('process' is provided by node)
 	var port = process.argv[2];
 
-	server.start(HOME_DIR + "/homepage.html", HOME_DIR + "/404.html", port, function() {
+	server.start(HOME_DIR, "/404.html", port, function() {
 		console.log("Server started");
 	});
 }());
