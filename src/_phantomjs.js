e65c8c51f8a643a6162bb5cf3c4674fbd7b8a7af
rchavarria <txingo@gmail.com>
2013-08-30 08:22:23 +0200

    added verifications to smoke test that runs phantomJS to run our code into a virtual browser. those verifications check that we are able to draw in our drawing area

--- src/_phantomjs.js
+++ src/_phantomjs.js
@@ -2,23 +2,46 @@
 /*global phantom, wwp, $ */
 
 (function() {
-	"use strict";
+    "use strict";
 
-	var page = require("webpage").create();
+    var page = require("webpage").create();
 
-	console.log("Hello world");
+    page.onConsoleMessage = function(message) {
+        console.log("CONSOLE: " + message);
+    };
 
-	page.open("http://localhost:5000", function(success) {
-		page.evaluate(inBrowser);
+    page.open("http://localhost:5000", function(success) {
+        try {
+            var error = page.evaluate(inBrowser);
+            if (error) {
+                console.log(error);
+                phantom.exit(1);
+            } else {
+                phantom.exit(0);
+            }
+        
+        } catch(err) {
+            console.log("Exception in PhantomJS code: " + err);
+            phantom.exit(2);
+        }
+    });
 
-		phantom.exit(0);
-	});
+    function inBrowser() {
+        try {
+            var drawingArea = new wwp.HtmlElement($("#drawingArea"));
+            drawingArea.mouseDown(10, 20);
+            drawingArea.mouseMove(50, 60);
+            drawingArea.mouseUp(50, 60);
 
-	function inBrowser() {
-		var drawingArea = new wwp.HtmlElement($("#drawingArea"));
-		drawingArea.mouseDown(10, 20);
-		drawingArea.mouseMove(50, 60);
-		drawingArea.mouseUp(50, 60);
-	}
+            var actual = JSON.stringify(wwp.drawingAreaCanvas.lineSegments());
+            var expected = JSON.stringify([[ "10", "20", "50", "60" ]]);
+
+            if (actual !== expected) return "lines drawn expected " + expected + " but was " + actual;
+            else return null;
+        
+        } catch(err) {
+            return "Exception in PhantomJS browser code: " + err;
+        }
+    }
 
 }());
