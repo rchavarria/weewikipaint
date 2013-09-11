7dc3d1a69ca9e2d05964e44ecff4c3023a07ac23
rchavarria <txingo@gmail.com>
2013-09-11 22:57:17 +0200

    added a new file, _run_server.js to share code between _smoke_test.js and Jakefile

---
+++ src/_run_server.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+/*jshint regexp:false*/
+
+(function() {
+	"use strict";
+
+	var child_process = require("child_process");
+	var fs = require("fs");
+	var procfile = require("procfile");
+
+	exports.runServer = function(callback) {
+		var commandLine = parseProcFile();
+		var serverProcess = child_process.spawn(commandLine.command, commandLine.options, {stdio: ["pipe", "pipe", process.stderr]});
+		serverProcess.stdout.setEncoding("utf8");
+		serverProcess.stdout.on("data", function(chunk) {
+			if (chunk.trim().indexOf("Server started") !== -1) callback(serverProcess);
+		});
+	};
+
+	function parseProcFile() {
+		var fileData = fs.readFileSync("Procfile", "utf8");
+		var webCommand = procfile.parse(fileData).web;
+		webCommand.options = webCommand.options.map(function(element) {
+			if (element === "$PORT") return "5000";
+			else return element;
+		});
+		return webCommand;
+	}
+
+}());
